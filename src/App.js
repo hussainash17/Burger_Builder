0f54f929ae1f626b1a13f0679647d43b7b54500b
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2020-05-02 02:05:00 +0600

    component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './components/containers/BurgerBuilder/BurgerBuilder'
+
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout>
+          <BurgerBuilder>
+            
+          </BurgerBuilder>
+        </Layout>
       </div>
     );
   }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Aux from '../../hoc/Aux'
+import classes from './Layout.css'
+
+const Layout = (props) => (
+    <Aux>
+        <div>
+            Toolbar, Sidebar, Backdrop
+        </div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+)
+
+export default Layout
